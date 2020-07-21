d373b813cce2e225b91557bcef20d4fbfaa39751
devluciano <61517478+devluciano@users.noreply.github.com>
2020-07-21 20:44:46 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar(){
+    window.open("https://globalabs.academy/");
+
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*var frutas = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
 /*var fruta = {nome:"maçã", cor:"vermelha"}
 console.log(fruta.nome);
 alert(fruta.cor);
