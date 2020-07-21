2309fbd1f1ed000790f0356a7be1e97e73c904f9
devluciano <61517478+devluciano@users.noreply.github.com>
2020-07-20 21:15:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
+/*var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
 
-var nome = "Luciano Silva";
-var idade = 45;
-var idade2 = 10;
+
+//var lista = ["maçã", "Pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Luciano Silva";
+//var idade = 45;
+//var idade2 = 10;
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2);
